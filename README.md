33cb5305073d5900cd89a97340f63035396e0359
Olli-Pekka Puolitaival <olli-pekka@puolitaival.fi>
2021-04-19 22:33:32 +0300

    Add kafka send logic

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 
 # Web site monitoring project
+
+TODO:
+* Send Kafta
+* Read Kafka
+* Send to Possu
+* Jenkinsfiles
+* Dockerization
+* pylinter
--- tests/test_web.py
+++ tests/test_web.py
@@ -3,18 +3,39 @@ import pytest
 import time
 import logging
 import json
+import os
+
+from kafka import KafkaProducer
 
 logger = logging.getLogger(__name__)
 
+# Read sites configuration from file
 sites = list()
-with open('config.json') as f:
+if not os.path.isfile('sites.json'):
+    raise Exception('sites.json is expected to locate in current working directory!')
+with open('sites.json') as f:
     sites = json.load(f)
 
 
-@pytest.mark.parametrize('name', list(sites))
-def test_web_site(name):
-    site = sites[name]
+@pytest.fixture(scope='session')
+def kafka_json_sender():
+    # TODO: Read address from configuration
+    producer = KafkaProducer(
+        bootstrap_servers='localhost:9092',
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
+    topic = 'my_channel'
 
+    def send(json_message):
+        logger.debug("Sending kafka: topic: {}, message: {}".format(topic, json_message))
+        producer.send(topic, json_message)
+
+    yield send
+    producer.close()
+
+
+@pytest.mark.parametrize('site', sites, ids=[x['name'] for x in sites])
+def test_web_site(site, kafka_json_sender):
     # Do request and measure time
     start_time = time.time()
     r = requests.get(site['url'])
@@ -24,9 +45,11 @@ def test_web_site(name):
     data = {
         'expected_status_code': site.get('expected_status_code'),
         'status_code': r.status_code,
-        'duration': duration,
+        'duration_ms': duration * 1000,
     }
     logger.debug('data: {}'.format(data))
+    kafka_json_sender.send(data)
+
     assert site.get('expected_status_code') == r.status_code
     if 'assert_text' in list(site):
         assert site['assert_text'] in r.text
