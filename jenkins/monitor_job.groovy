0aa3967446476eb64326d69770a70d944d0bfc96
Olli-Pekka Puolitaival <olli-pekka@puolitaival.fi>
2021-04-24 19:59:28 +0300

    Add test job

--- jenkins/monitor_job.groovy
+++ jenkins/monitor_job.groovy
@@ -1,11 +1,21 @@
 #!/usr/bin/env groovy
 /**
-* This job monitor web page by running every now and then
+* This job monitor web page by running every 5 minutes
+* How to use this:
+* 1. Put your configuration file in Jenkins secrets. docs: https://www.jenkins.io/doc/developer/security/secrets/
+* 2. Create Pipeline job in Jenkins
+* 3. Copy this file or configure jenkins to fetch this from git
+* 4. Trigger job in first time
+* Remember to put kafka_to_posrgres script running also
 */
 
 node('master'){
 
-    git(url:'git@github.com:OPpuolitaival/web_monitor.git', credentialsId:'ssh-key')
+    // Build every 5 minutes
+    properties([pipelineTriggers([cron('H/5 * * * *')])])
+    deleteDir()
+
+    git(url:'https://github.com/OPpuolitaival/web_monitor.git', branch:'main')
     withCredentials([file(credentialsId: 'config-file', variable: 'FILE')]) {
 
         stage('test'){
@@ -19,7 +29,7 @@ node('master'){
             """)
 
             // Make test report visible in Jenkins also
-            junit('junit.xml')
+            junit('pytests/junit.xml')
 
             if (exitCode == 1) {
                 echo "Failures in test execution"
