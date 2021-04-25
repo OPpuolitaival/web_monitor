e4c8c316371e98f5508380f21ef4798d48dc42fe
Olli-Pekka Puolitaival <olli-pekka@puolitaival.fi>
2021-04-25 14:02:36 +0300

    Reformat Jenkins files and add timeout for monitoring job

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,26 +1,26 @@
 #!/usr/bin/env groovy
 
 /**
-* This is doing basic checks in Jenkins when changes are done in branches or pull requests
-* Need to be configure in Jenkins multibranch pipeline
-* Documentation: https://www.jenkins.io/doc/book/pipeline/multibranch/
-*/
+ * This is doing basic checks in Jenkins when changes are done in branches or pull requests
+ * Need to be configure in Jenkins multibranch pipeline
+ * Documentation: https://www.jenkins.io/doc/book/pipeline/multibranch/
+ */
 
-def run_with_virtualenv(command){
+def run_with_virtualenv(command) {
     sh """#/bin/bash
         source temp_venv/bin/activate
         ${command}
     """
 }
 
-node('master'){
+node('master') {
 
     checkout scm
-    stage('Setup'){
+    stage('Setup') {
         sh 'python3 -m venv temp_venv'
         run_with_virtualenv('pip install -r requirements.txt')
     }
-    stage('Pylint'){
+    stage('Pylint') {
         run_with_virtualenv('pylint --ignore-patterns=temp_venv *')
     }
 }
--- jenkins/monitor_job.groovy
+++ jenkins/monitor_job.groovy
@@ -1,25 +1,27 @@
 #!/usr/bin/env groovy
 /**
-* This job monitor web page by running every 5 minutes
-* How to use this:
-* 1. Put your configuration file in Jenkins secrets. docs: https://www.jenkins.io/doc/developer/security/secrets/
-* 2. Create Pipeline job in Jenkins
-* 3. Copy this file or configure jenkins to fetch this from git
-* 4. Trigger job in first time
-* Remember to put kafka_to_posrgres script running also
-*/
+ * This job monitor web page by running every 5 minutes
+ * How to use this:
+ * 1. Put your configuration file in Jenkins secrets. docs: https://www.jenkins.io/doc/developer/security/secrets/
+ * 2. Create Pipeline job in Jenkins
+ * 3. Copy this file or configure jenkins to fetch this from git
+ * 4. Trigger job in first time
+ * Remember to put kafka_to_posrgres script running also
+ */
 
-node('master'){
+node('master') {
 
     // Build every 5 minutes
     properties([pipelineTriggers([cron('H/5 * * * *')])])
     deleteDir()
 
-    git(url:'https://github.com/OPpuolitaival/web_monitor.git', branch:'main')
-    withCredentials([file(credentialsId: 'config-file', variable: 'FILE')]) {
+    timeout(unit: 'MINUTES', time: 5) { // It is important that jobs are not hanging forever
 
-        stage('test'){
-            def exitCode = sh(returnStatus: true, script: """#/bin/bash
+        git(url: 'https://github.com/OPpuolitaival/web_monitor.git', branch: 'main')
+        withCredentials([file(credentialsId: 'config-file', variable: 'FILE')]) {
+
+            stage('test') {
+                def exitCode = sh(returnStatus: true, script: """#/bin/bash
                 python3 -m venv temp_venv
                 source temp_venv/bin/activate
                 pip install -r requirements.txt
@@ -28,16 +30,17 @@ node('master'){
                 pytest --junitxml=junit.xml --config config.json .
             """)
 
-            // Make test report visible in Jenkins also
-            junit('pytests/junit.xml')
+                // Make test report visible in Jenkins also
+                junit('pytests/junit.xml')
 
-            if (exitCode == 1) {
-                echo "Failures in test execution"
-                archiveArtifacts(artifacts:"*.log")
-            }
-            if (exitCode > 1) {
-                archiveArtifacts(artifacts:"*.log")
-                error "Pytest cannot run tests right. Check your configuration!"
+                if (exitCode == 1) {
+                    echo "Failures in test execution"
+                    archiveArtifacts(artifacts: "*.log")
+                }
+                if (exitCode > 1) {
+                    archiveArtifacts(artifacts: "*.log")
+                    error "Pytest cannot run tests right. Check your configuration!"
+                }
             }
         }
     }
